db92093755ad252781a82f2e8fcc9254b0829363
Giulio Sorrentino <numerone@fastwebnet.it>
2025-04-14 23:12:20 +0200

    ora l'url di github non viene più convertuto ad ogni pressione sul pulsante

--- wheelofnumfortune/InfoPage.xaml.cs
+++ wheelofnumfortune/InfoPage.xaml.cs
@@ -2,13 +2,15 @@ namespace wheelofnumfortune;
 
 public partial class InfoPage : ContentPage
 {
-	public InfoPage()
+    public static readonly Uri uri = new Uri("https://github.com/GiulianoSpaghetti/WheelOfNumFortune");
+
+    public InfoPage()
 	{
 		InitializeComponent();
 	}
 
     private async void OnSito_Click(object sender, EventArgs e)
     {
-        await Launcher.Default.OpenAsync(new Uri("https://github.com/GiulianoSpaghetti/WheelOfNumFortune"));
+        await Launcher.Default.OpenAsync(uri);
     }
 }
